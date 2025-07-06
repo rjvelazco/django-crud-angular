31b246cd53d4becc4a3180777b65a97a8f58fcf0
Rafael Velazco <rjvelazco21@gmail.com>
2025-07-06 17:54:02 -0400

    feat: Update Angular client with PrimeNG integration, new styles, and task listing enhancements

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
--- client/src/app/pages/tasks/listing/listing.ts
+++ client/src/app/pages/tasks/listing/listing.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+import { TableModule } from 'primeng/table';
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'tasks-listing',
-  imports: [],
+  imports: [TableModule, ButtonModule],
   templateUrl: './listing.html',
   styleUrl: './listing.css'
 })
-export class TasksListing {
+export class TasksListing implements OnInit {
+
+  protected readonly tasks = signal<Task[]>([]);
+  protected readonly httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient.get<Task[]>('http://localhost:8000/api/v1/tasks').subscribe((tasks) => {
+      this.tasks.set(tasks);
+    });
+  }
 
 }
