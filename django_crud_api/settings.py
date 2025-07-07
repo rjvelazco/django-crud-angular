8a1491c289704eafd30ed933bcdf87c1d7fa2726
Rafael Velazco <rjvelazco21@gmail.com>
2025-07-06 20:14:48 -0400

    feat: Update CORS settings to allow requests from localhost for development

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 
 # REST Framework settings
 REST_FRAMEWORK = {
